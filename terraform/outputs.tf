30857d1db7e343a2009f529648119736d7ce2c32
Tejasri2602 <tejasrimunjala2@gmail.com>
2026-06-15 13:06:19 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_a" {
+  value = aws_subnet.public_a.id
+}
+
+output "public_subnet_b" {
+  value = aws_subnet.public_b.id
+}
