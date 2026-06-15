e521f91dfbb15e0d3526663486aecc835b7fc14f
Tejasri2602 <tejasrimunjala2@gmail.com>
2026-06-15 18:35:41 +0530

    Update health_check.py

--- scripts/health_check.py
+++ scripts/health_check.py
@@ -1,1 +1,15 @@
 
+import requests
+
+URL = "http://localhost:5000/health"
+
+try:
+    response = requests.get(URL, timeout=5)
+
+    if response.status_code == 200:
+        print("Application Healthy")
+    else:
+        print("Application Unhealthy")
+
+except Exception as e:
+    print(f"Error: {e}")
